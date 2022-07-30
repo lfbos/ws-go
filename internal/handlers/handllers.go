6d1617fe555efe7250d8f2d6859f31ab45d33683
Luis Felipe Junior Boscan Nava <lboscannava@gmail.com>
2022-07-30 19:13:04 -0400

    Home and ws endpoint added + templates

---
+++ internal/handlers/handllers.go
@@ -0,0 +1,85 @@
+package handlers
+
+import (
+	"github.com/CloudyKit/jet/v6"
+	"github.com/gorilla/websocket"
+	"log"
+	"net/http"
+)
+
+var views = jet.NewSet(
+	jet.NewOSFileSystemLoader("./html"),
+	jet.InDevelopmentMode(),
+)
+
+var upgradeConnection = websocket.Upgrader{
+	ReadBufferSize:  1024,
+	WriteBufferSize: 1024,
+	CheckOrigin: func(r *http.Request) bool {
+		return true
+	},
+}
+
+// Home renders the home page
+func Home(w http.ResponseWriter, r *http.Request) {
+	err := renderPage(w, "home.jet", nil)
+	if err != nil {
+		log.Println(err)
+		return
+	}
+
+}
+
+type WebSocketConnection struct {
+	*websocket.Conn
+}
+
+// WsJsonResponse defines the response sent back from websocket
+type WsJsonResponse struct {
+	Action      string `json:"action"`
+	Message     string `json:"message"`
+	MessageType string `json:"message_type"`
+}
+
+type WsPayload struct {
+	Action   string              `json:"action"`
+	Username string              `json:"username"`
+	Message  string              `json:"message"`
+	Conn     WebSocketConnection `json:"-"`
+}
+
+// WsEndpoint upgrade connection to websocket
+func WsEndpoint(w http.ResponseWriter, r *http.Request) {
+	ws, err := upgradeConnection.Upgrade(w, r, nil)
+	if err != nil {
+		log.Println(err)
+		return
+	}
+
+	log.Println("Client connected to endpoint")
+
+	var response WsJsonResponse
+	response.Message = `<em><small>Connected to server</small></em>`
+
+	err = ws.WriteJSON(response)
+	if err != nil {
+		log.Println(err)
+		return
+	}
+}
+
+func renderPage(w http.ResponseWriter, tmpl string, data jet.VarMap) error {
+	view, err := views.GetTemplate(tmpl)
+	if err != nil {
+		log.Println(err)
+		return err
+	}
+
+	err = view.Execute(w, data, nil)
+
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
